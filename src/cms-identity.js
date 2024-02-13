bde229aeeeb78e5258668e12d5ad9b74fd557b1a
AndriiChubariev <andrii.chubariev+github@gmail.com>
2024-02-13 11:00:57 -0500

    feat: tries to fix upload bugs

--- src/cms-identity.js
+++ src/cms-identity.js
@@ -20,6 +20,6 @@ netlifyIdentityWidget.on(`init`, user => {
 
 // Boot on next tick to prevent clashes with css injected into StaticCMS
 // preview pane.
-setImmediate(() => {
+setTimeout(() => {
   netlifyIdentityWidget.init()
-})
+}, 0)
